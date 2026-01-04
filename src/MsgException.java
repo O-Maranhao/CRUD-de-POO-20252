1876bda8414ee01896cc11e3edc9c6f3297d459e
Maranhão <joaoffoli123>
2026-01-04 17:13:45 -0300

    Setimmo Update Exceptions alterados

--- src/MsgException.java
+++ src/MsgException.java
@@ -3,3 +3,13 @@ public class MsgException extends Exception{
         super(mensagem);
     }
 }
+class LivroIndisponivel extends Exception{
+    protected LivroIndisponivel(String mensagem){
+        super(mensagem);
+    }
+}
+class LimiteEmprestimo extends Exception{
+    protected LimiteEmprestimo(String mensagem){
+        super(mensagem);
+    }
+}
