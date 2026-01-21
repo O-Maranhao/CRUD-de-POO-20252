8918e70c71738cdda6844c5c3558dec02892e236
jeniferrcarolinaa <jeniferrcarolinaa@gmail.com>
2026-01-21 20:56:52 -0300

    Atualização da Jenifer.1 - Class Sistema, Exceptions e compilação ok

--- src/SistemaBiblioteca.java
+++ src/SistemaBiblioteca.java
@@ -11,6 +11,50 @@ public class SistemaBiblioteca {
         this.emprestimos = new ArrayList<>();
     }
 
+    public void removerLivro(String titulo) throws NaoEncontrado{
+        //procura - acha - remove
+        //n achou, lança erro
+        //percorrer livros cadastrados com o for
+        for(int i = 0; i < this.livros.size(); i++){
+            Livro l = livros.get(i); // pegar o livro do array
+            if(l.getTitulo().equals(titulo)){ // verificar se o livro digitado pelo usuario é o livro que tá no sistema
+                this.livros.remove(i); // remover o livro da lista
+                IO.println("Livro removido com sucessoo");
+                return;
+            }
+        }
+        throw new NaoEncontrado("fail: livro não encontrado no sistema");
+    }
+
+    public void removerUsuario(String nome) throws UsuarioException{
+        for(int i = 0; i < this.usuarios.size(); i++){
+            Usuario u = usuarios.get(i); // pegar o livro do array
+            if(u.getNome().equals(nome)){ // verificar se o livro digitado pelo usuario é o livro que tá no sistema
+                this.usuarios.remove(i); // remover o livro da lista
+                IO.println("Usuario removido com sucessoo.");
+                return;
+            }
+        }
+        throw new UsuarioException("fail: usuario não encontrado no sistema.");
+    }
+
+    public void editarLivro(String titulo, String novoTitulo, String novoAutor, String novoGenero, int novaQuantidade) throws LivroException{
+        //String titulo é pra acessar o livro a ser editado pelo titulo dele
+        for(int i = 0; i<this.livros.size(); i++){
+            Livro l = livros.get(i);
+            if(l.getTitulo().equals(titulo)){
+                l.setTitulo(novoTitulo);
+                l.setAutor(novoAutor);
+                l.setGenero(novoGenero);
+                l.setQuantidadeDisponivel(novaQuantidade);
+                IO.println("Livro editado com sucesso.");
+                return;
+            }
+        }
+
+        throw new LivroException("fail: livro nao encontrado.");
+    }
+
     // Menu principal (simples para exemplo)
     public void menuPrincipal() {
         System.out.println("=== Sistema da Biblioteca ===");
